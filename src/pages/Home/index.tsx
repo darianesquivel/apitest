286b6903aab404a1dbf32a9e869d49f9b42882c8
Darian ESquivel <darianesquivelf@gmail.com>
2023-02-11 15:09:44 -0300

    zustand state

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,39 @@
+import { Button, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { create } from "zustand";
+
+const useStyles = makeStyles(() => ({
+  formContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+const useSearchState = create((set: any) => ({
+  search: "",
+  setter: (value: any) => set((state: any) => ({ search: value, ...state })),
+}));
+
+console.log("useSearchState", useSearchState);
+
 function Home() {
-  return <h1>Homee</h1>;
+  const classes = useStyles();
+  const { search, setter } = useSearchState();
+
+  return (
+    <>
+      <form action="" className={classes.formContainer}>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          value={search}
+          onChange={() => setter(search)}
+        />
+        <Button>Search</Button>
+      </form>
+      <h1>{search}</h1>
+    </>
+  );
 }
 export default Home;
